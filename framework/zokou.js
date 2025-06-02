a0abcfa13f156eb7b7591813cb3875937d7e2869
⋆˚｡⋆୨✧୧˚ 𝑴𝒓 𝒘𝒊𝒔𝒆 ˚୨✧୧⋆｡˚⋆ <benardwachira49@gmail.com>
2025-06-02 03:34:23 +0300

    Add files via upload

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "🌎";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
@@ -14,4 +14,3 @@ function zokou(obj, fonctions) {
     return infoComs;
 }
 module.exports = { zokou, Module: zokou, cm };
-
