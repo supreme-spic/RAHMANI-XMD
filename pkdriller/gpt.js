c4d7a5e727b91e9ec7c64f6cea7885b292ed8b5e
Qartde <bonifacemd2525@gmail.com>
2025-06-12 14:54:51 +0300

    Delete pkdriller/gpt.js

--- pkdriller/gpt.js
+++
@@ -1,6 +0,0 @@
-const { zokou } = require('../framework/zokou');
-
-zokou({ nomCom: "hellow", reaction: "👋", categorie: "fun" }, async (dest, zk, commandeOptions) => {
-  const { repondre } = commandeOptions;
-  repondre('Habari yako! Karibu kwenye bot yako ya Rahmani_Xmd.');
-});
